0af48dedf29a07b05c08b39d54b2491e59b6d57d
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-23 11:03:50 +0530

    Initial Commit

---
+++ ACM -2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACM -2
+//
+//  Created by Aaryan Kothari on 23/03/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
