c1c2c1b1fe740b4357ed8974dbcd48dd2cf22ae4
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-23 11:58:01 +0530

    ProfileVC added

---
+++ ACM -2/ProfileVC.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileVC.swift
+//  ACM -2
+//
+//  Created by Aaryan Kothari on 23/03/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileVC_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileVC()
+    }
+}
