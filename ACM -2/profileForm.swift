44a21d622c160d9ad1c9579b01ecf36da522ab29
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-23 11:24:45 +0530

    initial commit

---
+++ ACM -2/profileForm.swift
@@ -0,0 +1,21 @@
+//
+//  profileForm.swift
+//  ACM -2
+//
+//  Created by Aaryan Kothari on 23/03/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct profileForm: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+struct profileForm_Previews: PreviewProvider {
+    static var previews: some View {
+        profileForm()
+    }
+}
